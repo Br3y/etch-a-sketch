a44dd136a52c94ef6b1fe6c72f48213b0ebfddd1
Aubrey Lizardo <116300259+Br3y@users.noreply.github.com>
2023-09-19 15:07:42 +0800

    feat: get container, create grid div and style it

---
+++ main.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container");
+
+const div = document.createElement("div");
+div.classList.add("grids");
+div.style.width = "100%";
+div.style.height = "100%";
+div.style.backgroundColor = "gray";
+container.appendChild(div);
