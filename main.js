0160c20bd831b572ac8087111cc65fbea1ee54f1
Aubrey Lizardo <116300259+Br3y@users.noreply.github.com>
2023-09-22 14:30:09 +0800

    fix: spaces for better readability

--- main.js
+++ main.js
@@ -1,54 +1,94 @@
 // DECLARATION
 const container = document.querySelector(".container");
+
 let range = document.querySelector("#range");
+
 let gridSize = range.value;
+
 
 // CHANGING gridsize in label
 const label = document.querySelector("label");
+
 range.addEventListener("mousemove", ()=>{
+
     label.textContent = range.value;
+
 })
+
 
 // IT WILL CALCULATE GRIDNUMBER * GRIDNUMBER USING FOR LOOP
 function createDivs(num) {
+
     for(let row = 0; row < num; row++){
+
         for(let col = 0; col < num; col++){
+
             let div = document.createElement("div");
+
             div.classList.add("grids");
+
             div.style.width = `calc(100% / ${gridSize})`;
+
             container.appendChild(div);
+
         }
+
     }
+
 }
+
+
 // CALL createDivs function
 createDivs(gridSize)
 
 // DECLARE GRIDS FOR BUTTON
 const grids = document.querySelectorAll(".grids");
+
 // IT WILL MAKE SINGLE GRID BLACK WHEN HOVER
 function changeToBlackButton(grids){
+
     grids.forEach((grid) => grid.addEventListener("mouseover", ()=>{
+
         grid.style.backgroundColor = "black";
+
     }));
+
 }
+
 changeToBlackButton(grids)
+
 
 // RESET BUTTON
 const resetButton = document.querySelector("#reset");
+
 resetButton.addEventListener("click", ()=>{
+
     grids.forEach((grid) => grid.style.backgroundColor = "white");
+
 })
+
+
 // RESET TO BLACK BUTTON
 const blackButton = document.querySelector("#black");
+
 blackButton.addEventListener("click", ()=>{
+
     changeToBlackButton(grids)
+
 })
+
 
 // MAKING GRID COLORED RANDOMLY WHEN RGB BUTTON IS CLICK
 const rgb = document.querySelector("#rgb");
+
 rgb.addEventListener("click", ()=>{
+
     grids.forEach((grid) => grid.addEventListener("mouseover", ()=>{
+
         let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
         grid.style.backgroundColor = `#${randomColor}`;
+
     }));
+    
 })
